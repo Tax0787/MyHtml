3666aa116a2e3f6f5f032076d983a1cce6b2e301
Tax0787 <in_huan08@naver.com>
2023-05-20 20:46:32 +0900

    Hoxy

--- MakeHtml.py
+++ MakeHtml.py
@@ -332,7 +332,7 @@ def test():
             }
         }
     )
-    data = HTML(head(('', ''), title_bar = ('style',None), id = 'head_tag', title = '시험적 웹사이트', PyScript = True), body(value, id = 'body_tag'), lang = 'ko')
+    data = HTML(head(('', ''), title_bar = ('link','HTMLSource/상단바2.css'), id = 'head_tag', title = '시험적 웹사이트', PyScript = True), body(value, id = 'body_tag'), lang = 'ko')
     print(data)
     with open('index.html', 'w', encoding = 'utf-8') as f:
         f.write(data)
